587232f8a6f3680f01351584b14ffe5e33cf0df6
janhrastnik <jan.hrastnik2@gmail.com>
2024-12-01 23:34:03 +0100

    skripti za generiranje grafov premera dve in grafov tipa (SB)

---
+++ diam_two_gen.py
@@ -0,0 +1,13 @@
+from sage.all import *
+import time
+
+start = time.time()
+with open("diam_two_graphs.txt", "a") as F:
+    for i in range(1, 11):
+        for g in graphs.nauty_geng(f"{i} -c"):
+            if g.diameter() == 2:
+                F.write(str(g.to_dictionary()) + "\n")
+
+end = time.time()
+diff = round(end-start, 2)
+print("Done! Took ", diff, " seconds to run.")
