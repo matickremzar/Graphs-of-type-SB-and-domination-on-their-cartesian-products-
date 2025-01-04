cc59554c3ecf139af510fe0f29d8e89fb16d5963
janhrastnik <jan.hrastnik2@gmail.com>
2025-01-04 20:19:02 +0100

    popravil x-os v histogramih

--- programi/plot_domination_numbers.py
+++ programi/plot_domination_numbers.py
@@ -6,16 +6,18 @@ data = []
 with open("../podatki/sb_cartesian_products_dominating_numbers_below_9.txt", "r") as F:
     for line in F:
         g1, g2, d = line.rstrip().split()
-        data.append(int(d))
+        data.append(int(d)-0.5)
 
 bins = np.arange(min(data), max(data)+2)
+bins_ticks = np.arange(min(data)-0.5, max(data)+1.5)
 
 print(Counter(data))
 
 fig, ax = plt.subplots()
 
 ax.hist(data, bins=bins)
-plt.xticks(bins)
+plt.xticks(bins_ticks)
+
 ax.set_title("Dominacijska števila kartezičnih produktov grafov tipa (SB) z 8 vozlišči ali manj")
 plt.yscale("log")
 plt.xlabel('Dominacijsko število')
--- programi/plot_domination_numbers_upper_sampling.py
+++ programi/plot_domination_numbers_upper_sampling.py
@@ -6,16 +6,18 @@ data = []
 with open("../podatki/sb_cartesian_products_dominating_numbers_upper_sampled.txt", "r") as F:
     for line in F:
         g1, g2, d = line.rstrip().split()
-        data.append(int(d))
+        data.append(int(d)-0.5)
 
 bins = np.arange(min(data), max(data)+2)
+bins_ticks = np.arange(min(data)-0.5, max(data)+1.5)
 
 print(Counter(data))
 
 fig, ax = plt.subplots()
 
-ax.hist(data, bins=bins)
-plt.xticks(bins)
+ax.hist(data, bins=bins, align="mid")
+plt.xticks(bins_ticks)
+
 ax.set_title("Dominacijska števila kartezičnih produktov grafov tipa (SB) z 10 vozlišči,\n naključno izbrani brez ponavljanja, 10 000 vzorcev")
 plt.yscale("log")
 plt.xlabel('Dominacijsko število')
