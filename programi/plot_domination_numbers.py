ddae1610edec687e99559c095f8508e4f8eb5923
janhrastnik <jan.hrastnik2@gmail.com>
2025-01-04 14:12:21 +0100

    olepšave

--- programi/plot_domination_numbers.py
+++ programi/plot_domination_numbers.py
@@ -8,15 +8,13 @@ with open("../podatki/sb_cartesian_products_dominating_numbers_below_9.txt", "r"
         g1, g2, d = line.rstrip().split()
         data.append(int(d))
 
-n_bins = 8
+bins = np.arange(5, 13)
 
-bins = np.arange(5, 13)  # Creates bins [1, 2, ..., 9]
-
-#print(Counter(data))
+print(Counter(data))
 
 fig, axs = plt.subplots()
 
 axs.hist(data, bins=bins)
-plt.xticks(bins[:-1])  # Show ticks for each unique value
+plt.xticks(bins[:-1])
 
 plt.show()
